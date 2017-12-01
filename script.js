281ea3d1ee175ec4a5707ce0ae24067eec410129
Sofie Staal <minasofiestaallillelien@Minas-MacBook-Pro.local>
2017-12-01 13:36:01 +0100

    Fått lagt inn det meste av koden i flexbokser, drop down menyen gjenstår

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var appImg = document.querySelector('#mockup');
+/*var appImg = document.querySelector('#mockup');
 var appPos = appImg.offsetTop;
 
 
@@ -14,4 +14,27 @@ window.addEventListener('scroll', function () {
         appImg.style.transition = ".5s"
         appImg.style.opacity = '1'
     }
-})
+})*/
+
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height() - 700;
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$(window).scroll(function () {
+    $('.fadeItem').each(function () {
+        var isInView = isScrolledIntoView(this);
+        if (isInView) {
+            $(this).attr('data-in-view', 'true');
+        } else {
+            //$(this).attr('data-in-view', 'false');
+        }
+    });
+
+});
